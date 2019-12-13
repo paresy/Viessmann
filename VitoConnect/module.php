b98399fffa5c2f10a6222a0edba79f5f861a4b28
Sascha <starlein@users.noreply.github.com>
2019-12-13 15:48:45 +0100

    Fix cs and comment

--- VitoConnect/module.php
+++ VitoConnect/module.php
@@ -344,9 +344,10 @@ class VitoConnect extends IPSModule
                         case 'week':
                         case 'month':
                         case 'year':
-                            // Fetching values only if device can produce electricity
-                            if (count($property->value)>1)
+                            //If array has only single element then property is unsupported from device
+                            if (count($property->value) > 1) {
                                 $updateVariable($entity->class[0], $name, $property->type, $property->value[0] /* 0 = current period */, 'Electricity');
+                            }
                             break;
                         default:
                             $this->SendDebug($name, $entity->class[0] . ' = ' . print_r($property, true), 0);
